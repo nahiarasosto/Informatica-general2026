dcd3194a68ad1fe3e35b42e12cfc6790c14d4f25
NahiaraSosto <nahiarasosto@gmail.com>
2026-05-30 15:37:34 -0300

    Add files via upload

---
+++ practica-15/javaScript/script.js
@@ -0,0 +1,21 @@
+//Console.log
+
+console.log ('ariables');
+
+//Variables
+
+let DatosPersonales = {
+    Nombre: 'Nahiarita'/n,
+    Apellido: 'Sostoo',
+};
+
+let Edad = '18';
+
+const Facu = 'Universidad Nacional de las artes';
+
+//Modificaciones
+
+Nombre = 'Nahiu';
+Apellido = 'Sosto';
+Edad = '10 + 8';
+Facu = 'UNA';
